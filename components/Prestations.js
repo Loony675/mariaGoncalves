7b17a8c1c26465a19180644fffcea9a2f125c4c7
Loussot <theo.loussot@gmail.com>
2023-04-06 12:00:50 +0200

    text component prestations

--- components/Prestations.js
+++ components/Prestations.js
@@ -1,31 +1,48 @@
-import React from 'react'
-import styles from '../styles/Prestations.module.css'
+import React from "react";
+import styles from "../styles/Prestations.module.css";
 
 export default function Prestations() {
   return (
     <div className={styles.mainContainer}>
-      <h1 className={styles.title}>Prestations</h1>
+      <h1 className={styles.title}>Mes formations pour vous accompagner </h1>
       <div className={styles.container1}>
-        <img src="" className={styles.imgPortrait} alt='' />
+        {/* <img src="" className={styles.imgPortrait} alt="" /> */}
         <div className={styles.bioContainer}>
           <p>
-            Je suis Maria Goncalves, Auxiliaire de puériculture diplômée d'Etat.
+            Je suis certifiée au massage parents-bébé par l'Association
+            Internationale de Massage pour Bébé (IAIM) depuis 2013.
           </p>
           <p>
-            20 ans de regards et de soins attentifs, offerts aux enfants, m'ont
-            amené à choisir et développer l'art du toucher, l'épanouissement des
-            touts-petits et le bien-être parents-bébés.
+            Le massage des bébés est un art traditionnel indien qui contribue à
+            relaxer, soulager, stimuler et renforcer le lien avec son
+            tout-petit.
           </p>
           <p>
-            Sensible à l'accueil au monde du bébé, comme à son besoin vital
-            d'affection et de sécurité, je sensibilise les nouveaux parents à
-            l'art du toucher du nourrisson, à la compréhension de ses besoins
-            essentiels, et enseigne avec douceur un riche partage, pour une
-            détente familiale, et un nouvel éveil des sens vécu entre bébé, papa
-            et maman.
+            Ma curiosité et un intérêt de cœur m'ont plongé dans cet univers
+            maman-bébé autour du maternage proximal, l'éducation positive et
+            bienveillante, la communication non-violente, tous les moyens
+            permettant d'accompagner la maman et son tout-petit avec douceur.
+          </p>
+          <p>
+            Pour poursuivre dans cet univers doux et bienveillant, je suis
+            Facilitatrice de cercles mamans-bébés depuis 2021.
+          </p>
+          <p>
+            Ces cercles sont un temps sacré dédié à la maman et à son tout-petit
+            pour la création du lien avec son bébé mais aussi avec les autres
+            mamans participantes.
+          </p>
+          <p>
+            Je suis par ailleurs Praticienne Reiki, soin énergétique doux et
+            méditatif qui permet de rééquilibrer le corps et apaiser le mental
+            pour plus de mieux-être et de reconnexion à soi.{" "}
+            <p>
+              Ce soin s'adresse aux mamans mais aussi à toutes les personnes le
+              souhaitant.
+            </p>
           </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
